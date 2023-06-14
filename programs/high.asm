702a9311bc82a48adfd015f65388ce8a4b4865c2
slottwo <khaled.phg+github@gmail.com>
2023-06-14 15:53:41 -0300

    assemble version of high algorithm

--- programs/high.asm
+++ programs/high.asm
@@ -1,14 +1,21 @@
-0 b100
-1 1201
-2 b320 
-3 1f0e 
-4 8021 
-5 3221 
-6 b420 
-7 7534 
-8 1f0b 
-9 8050 
-a 2340 
-b a003 
-c c023 
-d a00d
+.data
+    size: .word 6
+    array: .word 10, 5, 8, 15, 3, 20
+
+.text
+    lw $1 $0[0]     # size
+    li $2 1         # i = 1
+    lw $3 $2[0]     # m=v[0]
+LOOP:
+    li $f END
+    beq $2 $1      
+    addi $2 $2 1   # i++
+    lw $4 $2[0]
+    stl $5 $3 $4   # r=m<v[i]
+    li $f ELSE
+    ben $5 $0
+    add $3 $4 $0
+ELSE:
+    j LOOP
+END:
+    sw $2[0] $3
